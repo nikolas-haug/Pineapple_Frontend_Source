fe9afb6b7316e9381370b63dc96043944ebfd0db
Nikolas Haug <nikohaug21@gmail.com>
2020-09-03 10:47:55 -0500

    add slider nav css and js

--- js/slider-nav.js
+++ js/slider-nav.js
@@ -1,21 +1,10 @@
 (function() {
 	const toggler = document.querySelector('.slider-menu__toggler');
 	const menu = document.querySelector('.slider-menu');
-	
-	const span_togglers = document.querySelector('.slider-menu__toggler span');
-	
-	/*
-	 * Toggles on and off the 'active' class on the menu
-	 * and the toggler button.
-	 */
-	toggler.addEventListener('click', () => {
-		toggler.classList.toggle('active');
-		menu.classList.toggle('active');
-	
-		document.body.classList.toggle('dimmed');
-	});
-	
-	// check for all children of an element
+	const spanTogglers = document.querySelector('.slider-menu__toggler span');
+	const menuItems = document.querySelectorAll('a[href^="#section"]');
+
+	// Helper Function - check for all children of an element
 	function isDescendantOrSelf(parent, child) {
 		let node = child;
 		while (node != null) {
@@ -26,17 +15,32 @@
 		}
 		return false;
 	}
+	/*
+	 * Toggles on and off the 'active' class on the menu
+	 * and the toggler button, and the 'dimmed' class on the body
+	 */
+	function toggleMenuClasses() {
+		toggler.classList.toggle('active');
+		menu.classList.toggle('active');
+		document.body.classList.toggle('dimmed');
+	}
+	toggler.addEventListener('click', () => {
+		toggleMenuClasses();
+	});
+	menuItems.forEach(item => {
+		item.addEventListener('click', () => {
+			if(-1 !== menu.className.indexOf('active')) {
+				toggleMenuClasses();
+			}
+		});
+	});
 	
+	// Close the slider menu if a click is not the parent node or any of its children
 	window.addEventListener('mouseup', function (event) {
 		const root = menu;
-	
-		if (-1 !== toggler.className.indexOf('active') && !isDescendantOrSelf(root, event.target) && event.target != span_togglers && event.target != toggler) {
-			// console.log('Passed!');
-			toggler.classList.toggle('active');
-			menu.classList.toggle('active');
-    
-            // Need to add this class in base if used
-			document.body.classList.toggle('dimmed');
+		console.log(event.target.tagName);
+		if (-1 !== toggler.className.indexOf('active') && !isDescendantOrSelf(root, event.target) && event.target != spanTogglers && event.target != toggler && event.target.tagName != 'A') {
+			toggleMenuClasses();
 		}
 	});
 })();
